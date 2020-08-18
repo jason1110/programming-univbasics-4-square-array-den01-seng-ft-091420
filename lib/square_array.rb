45567f39900114bf4514fa79fe199d92239acac9
Jason Easterly <jason.easterly11@gmail.com>
2020-08-18 22:37:31 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,8 @@ def square_array(array)
  counter = 1
  new_array = []
   while counter < array.index do
-  new_array.push(array[index]**)
+  new_array.push array[index]**
+  counter += 1
 end
   new_array
 end
